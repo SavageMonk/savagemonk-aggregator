da2bdf9c00b565052190cab1fa170884190d5c81
Nighthawk1210 <nighthawk1210coc@gmail.com>
2020-11-25 03:59:08 +0530

    =vedantcomputers

---
+++ primeabgb.js
@@ -0,0 +1,75 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function search(query) {
+    searchQuery = query.split(" ").map(term => encodeURIComponent(term)).join('+');
+    return searchAllPages(searchQuery);
+}
+
+async function searchAllPages(query, page) {
+    let { formattedItems, noOfPages } = await searchSinglePage(query, 1);
+
+    if (noOfPages > 1) {
+        promisesList = [];
+
+        for (let i = 2; i <= noOfPages; i++) {
+            promisesList.push(searchSinglePage(query, i));
+        }
+
+        return Promise.all(promisesList)
+            .then(itemsArray => {
+                itemsArray.forEach(items => {
+                    formattedItems.push(...items);
+                });
+                return formattedItems;
+            });
+
+    } else {
+        return formattedItems;
+    }
+
+
+}
+
+async function searchSinglePage(searchQuery, page) {
+    let formattedItems = [];
+    response = await axios.get(`https://www.primeabgb.com/page/${page}/?post_type=product&s=${searchQuery}`);
+    const $ = cheerio.load(response.data);
+    let items = $('li.product-item');
+    // console.log(items);
+    items.each((index, val) => {
+        // console.log($('span.price', val).find('ins').text().trim());
+        let price = Number($('span.price', val).find('ins').text().trim().split(',').join('').replace('₹',''));
+        let oldPrice = Number($('span.price', val).find('del').text().trim().split(',').join('').replace('₹',''));
+        let discountPercentage = ((oldPrice - price)/oldPrice) * 100;
+        // console.log({index, price});
+        formattedItems.push({
+            name: $('h3.product-name', val).text().trim(),
+            price,
+            discountPercentage,
+            url: $('h3.product-name', val).find('a').attr('href').trim(),
+            img: $('img.attachment-post-thumbnail', val).attr('src')
+        })
+    });
+
+
+    if (page == 1) {
+        let pagesUl = $('ul.page-numbers');
+        let noOfPages;
+        
+        try {
+            noOfPages = pagesUl.find('a.page-numbers').not('.next').last().text();
+        } catch (err) {
+            noOfPages = 1;
+        }
+
+        noOfPages = String(noOfPages).length==0?1:noOfPages;
+        return { formattedItems, noOfPages };
+    } else {
+        return formattedItems;
+    }
+}
+
+// search("3700x");
+
+module.exports = search;
--- vedantcomputers.js
+++ vedantcomputers.js
@@ -11,15 +11,18 @@ async function search(query) {
 
 async function searchAllPages(searchQuery) {
     let formattedItems = [];
-    response = await axios.get(`https://www.vedantcomputers.com/index.php?route=product/search&search=${searchQuery}&limit=100`);
+    response = await axios.get(`https://www.vedantcomputers.com/index.php?route=product/search&search=${searchQuery}&limit=1000`);
     const $ = cheerio.load(response.data);
     items = $('.product-thumb')
     items.each((index, val) => {
+        let img = $('.image', val).find('img').attr('data-src');
+        let imgHighRes = img.replace('250x250', '500x500');
         formattedItems.push({
             name: $('.name', val).text().trim(),
             price: $('.price-normal', val).text().trim(),
             url: $('.name', val).find('a').attr('href'),
-            img: $('.image', val).find('img').attr('data-src')
+            img,
+            imgHighRes
         })
     });
 
